591c1ff1cac9786755e872d664e24da4c7ff58ff
kejinzhu <34179222+kejinzhu@users.noreply.github.com>
2022-06-21 14:18:08 +0800

    Jinzhu.ke/add examples test (#4)
    * fix registry bug
    
    * add react sub app
    
    * test react root
    
    * update example
    
    * update examples
    
    * feat: update
    
    * feat: update subapp vue path
    
    * feat: update starter
    
    * feat: update
    
    * feat: update test
    
    * feat: update examples
    
    Co-authored-by: Jinzhu Ke <jinzhu.ke@shopee.com>
    Co-authored-by: huchao <huchao@shopee.com>

---
+++ sandbox/child-vue/src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <h1>app1 home page</h1>
+    <div @click="goReact">to main react App</div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  },
+  methods: {
+    goReact() {
+      window.history.pushState(null, null, '/child-react18/xxxx');
+    }
+  }
+}
+</script>
---
+++ switcher/child-react18/config-overrides.js
@@ -0,0 +1,6 @@
+module.exports = function override(config) {
+  config.output.library = 'child-react18';
+  config.output.libraryTarget = 'umd';
+  config.output.publicPath = 'http://localhost:3000/child-react18';
+  return config;
+};
